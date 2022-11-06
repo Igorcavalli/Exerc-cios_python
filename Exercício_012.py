f2bc309fdcd3147c6352691d69a04f99ab12bbf1
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 09:11:35 -0300

    Add files via upload

---
+++ Exercício_012.py
@@ -0,0 +1,10 @@
+"""Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+usando a seguinte fórmula: (72.7*altura) - 58"""
+
+print("VAMOS CALCULAR SEU PESO IDEAL?")
+altura= float(input("Para isso, digite sua altura: "))
+
+peso_ideal= (72.7*altura)-58
+
+print("Seu peso ideal é:",peso_ideal)
+
