ed09580c07660fb8d2e4d0a597b7eca802f7951c
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 09:07:45 -0300

    Add files via upload

---
+++ Exercício_007.py
@@ -0,0 +1,12 @@
+"""Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário."""
+
+print("Vamos calcular a área do quadrado?")
+
+lado = float(input("escreva o valor do lado do quadrado. "))
+area = float(lado * lado) 
+
+print(" A área do quadrado é:", area)
+
+print("mas, nós queremos o dobro desse valor. Então o valor que nós buscamos é:", area * 2)
+
+print("obrigado!")
