63b14d9747f8cf0922128ebcb5336ba2dd0d111f
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 09:07:11 -0300

    Add files via upload

---
+++ Exercício_006.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
+
+print("Vamos calcular a área de um círculo?")
+
+pi = 3.14
+raio = float(input("informe o RAIO "))
+raio2 = float(raio * raio)
+
+print("A Área desse círculo e:", raio2 * pi)
