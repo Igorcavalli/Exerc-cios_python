74905a9d52c774dfc32a7b6c6c61df5a3465e38d
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 08:35:58 -0300

    Add files via upload

---
+++ Exercicio_002.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que peça um número e então mostre a mensagem O número 
+informado foi [número]."""
+
+from turtle import clear
+
+
+resultado=input("Por favor, digite um número ")
+print("O número informado foi:", resultado)
