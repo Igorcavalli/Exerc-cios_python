f590c121cb60fb94d4a239bffc1c2fe0f7bef526
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 08:36:25 -0300

    Add files via upload

---
+++ Exercício_003.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+
+print("Escreva dois números:")
+numero1 = float(input("escreva o primeiro "))
+numero2 = float(input("escreva o segundo "))
+s = numero1 + numero2
+print("a soma entre {} e {} é igual a {}!".format(numero1, numero2, s))
