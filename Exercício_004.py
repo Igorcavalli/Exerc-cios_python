35c00ebff2fd7e18792ad3243cb35b76077cba2c
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 08:36:53 -0300

    Add files via upload

---
+++ Exercício_004.py
@@ -0,0 +1,18 @@
+"""Faça um Programa que peça as 4 notas bimestrais e mostre a média."""
+
+
+
+print("para obter a média das notas digite as:")
+
+b1 = float(input("Informe a nota do primeiro bimestre: "))
+
+b2 = float(input("Informe a nota do segundo bimestre: "))
+
+b3 = float(input("Informe a nota do terceiro bimestre: "))
+
+b4 = float(input("Informe a nota do quarto bimestre "))
+
+media = float(b1+b2+b3+b4)/4
+
+print("Sua média é:",media)
+
