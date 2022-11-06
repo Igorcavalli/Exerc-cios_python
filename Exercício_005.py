1130410bbcbf03deab926917938e4947c2412c02
Igorcavalli <117319163+Igorcavalli@users.noreply.github.com>
2022-11-06 08:37:18 -0300

    Add files via upload

---
+++ Exercício_005.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que converta metros para centímetros"""
+
+print('Converta a unidade de medidas METROS para CENTIMETROS.')
+
+centimetros = 100 
+
+metros = float(input("Coloque o valor em metros:" ))
+print("O valor em Centimetros é:" ,metros*centimetros)
